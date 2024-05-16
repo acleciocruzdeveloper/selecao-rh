3857af75d5c48fdb065e22f4eb45f404d739af59
acleciocruz <aclecioscruz@gmail.com>
2024-05-16 08:45:30 -0300

    logica de atendimento para auxiliar rh na selecao de candidatos com conseguiram atendimeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] candidatos = {"FELIPE", "JULIA", "MARCIA", "MARIA", "PAULO", "AUGUSTO"};
+        for (String candidato :
+                candidatos) {
+            entrandoEmContato(candidato);
+        }
     }
+
+    private static void entrandoEmContato(String candidato) {
+        int numeroTentativas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando){
+                numeroTentativas++;
+            }else {
+                System.out.println("CONTATO REALIZADO COM SUCESSO, COM O CANDIDATO " + candidato);
+            }
+        }while (continuarTentando && numeroTentativas < 3);
+
+        if (atendeu){
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA TENTATIVA " + numeroTentativas);
+        }else {
+            System.out.println("NAO CONSEGUIMOS CONTATO COM O CANDIDATO " + candidato + ", NUMERO DE TENTATIVAS "  + numeroTentativas);
+        }
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
 }
